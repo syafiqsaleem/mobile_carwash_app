a980d5c9b8e00d60cc3178198c19922131c23f8d
syafiqsaleem <19syafiq89@gmail.com>
2021-06-06 14:39:31 +0800

    models modifiedd

---
+++ models/collection.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+
+const collectionSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    max: 100,
+  },
+  username: {
+    type: String,
+    required: true,
+    max: 100,
+  },
+  image: String,
+  description: String,
+  likes: [String],
+  comments: [
+    {
+      username: {
+        type: String,
+        required: true,
+        max: 100,
+      },
+      content: {
+        type: String,
+        required: true,
+        max: 100,
+      },
+      created_at: {
+        type: Date,
+        required: true,
+        default: Date.now,
+      },
+      updated_at: {
+        type: Date,
+        required: true,
+        default: Date.now,
+      },
+    },
+  ],
+  customChoice: {},
+  created_at: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  updated_at: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+});
+
+const CollectionModel = mongoose.model("Collection", collectionSchema);
+
+module.exports = CollectionModel;
